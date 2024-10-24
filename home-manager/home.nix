8cf11e5c3418e50191ee9113567b5ea70738109b
John K. Ferguson <witandcharm@gmail.com>
2024-10-23 22:18:49 -0400

    feat(home): add git configuration via home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,7 +1,6 @@
 { lib, pkgs, ... }:
 {
   home = {
-    # Install packages from https://search.nixos.org/packages
     packages = with pkgs; [
       hello
       git
@@ -9,14 +8,69 @@
 
     username = "jkf";
     homeDirectory = "/home/jkf";
-
-    # Don't ever change this after the first build.
-    # It tells home-manager what the original state schema
-    # was, so it knows how to go to the next state.  It
-    # should NOT update when you update your system!
     stateVersion = "23.11";
   };
 
-  # Enable home-manager to manage itself
+  programs.git = {
+    enable = true;
+    userName = "John K. Ferguson";
+    userEmail = "witandcharm@gmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = false;
+
+      core = {
+        # 'code --wait' sets VSCode as the git editor
+        # The '--wait' flag is crucial - it makes the git command wait
+        # until you close the file in VSCode. Without this:
+        # - Git might continue before you finish editing
+        # - Commit messages might be empty
+        # - Interactive operations might fail
+        editor = "code --wait";
+
+        # autocrlf = input handles line ending conversions:
+        # - When checking out code: No conversion
+        # - When committing code: Converts CRLF (Windows) to LF (Unix)
+        # This is important in WSL2 because Windows uses CRLF line endings
+        # while Linux uses LF. Setting to 'input' prevents conflicts.
+        autocrlf = "input";
+      };
+
+      color.ui = "auto";
+
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+
+        # 'unstage' removes files from the staging area
+        # equivalent to 'git reset HEAD <file>'
+        unstage = "reset HEAD --";
+
+        # 'last' shows the details of your most recent commit
+        # equivalent to 'git log -1 HEAD'
+        last = "log -1 HEAD";
+
+        # 'lg' creates a prettier git log with a graph
+        # - %h: abbreviated commit hash
+        # - %d: ref names (branch, tags)
+        # - %s: commit subject (message)
+        # - %cr: committer date, relative
+        # - %an: author name
+        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
+
+      help = {
+        # When you mistype a command, git will correct and execute it
+        # Example: 'git stauts' will run 'git status'
+        # The value 1 means it waits 0.1 seconds before running the corrected command
+        # This gives you a chance to see what command is actually being run
+        autocorrect = 1;
+      };
+    };
+  };
+
   programs.home-manager.enable = true;
 }
